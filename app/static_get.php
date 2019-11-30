7beafddeb07e87cb377a92eeaaffcba2caec1720
Franz Vanderlinde <franzvanderlinde@riseup.net>
2019-11-30 15:05:18 -0500

    updating static getter

--- app/static_get.php
+++ app/static_get.php
@@ -4,18 +4,31 @@ require_once "static_config.php";
 $config = static_config();
 
 $path = explode('/', $_GET['q']);
+
+$index = 0;
+$lang = '';
+switch ($path[1]) {
+  case 'index':
+  case 'about':
+  case 'chapters':
+    $index = 1;
+    $lang = '/' . $path[0];
+    array_shift($path);
+    break;
+}
+
 switch ($path[0]) {
   case 'index':
   case 'about':
-    $url = $config['backend_url'];
+    $url = $config['backend_url'] . $lang;
     break;
   case 'chapters':
     if (count($path) == 1) {
-      $url = $config['backend_url'];
+      $url = $config['backend_url'] . $lang;
       break;
     }
     elseif (count($path) == 2 && is_numeric($path[1])) {
-      $url = $config['backend_url'] . '/node/' . $path[1];
+      $url = $config['backend_url'] . $lang . '/node/' . $path[1];
       break;
     }
     // Intentionally fallback to default fom here
